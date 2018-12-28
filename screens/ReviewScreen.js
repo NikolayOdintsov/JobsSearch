1560b635399268dbeb40812113b37d5ad04f7aae
Nikolay Odintsov <nikolay.odintsov@akelius.de>
2018-12-28 20:23:43 +0100

    Improved header

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -8,7 +8,19 @@ class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => {
         return {
             headerTitle: 'Review Jobs',
-            headerRight: (<Button title="Settings" onPress={() => navigation.navigate('settings')}/>)
+            headerRight: (
+                <Button
+                    title="Settings"
+                    onPress={() => navigation.navigate('settings')}
+                    backgroundColor="rgba(0,0,0,0)"
+                    //ios color
+                    color="rgba(0,122,255,1)"
+                />
+            ),
+            headerStyle: {
+                marginTop: Platform.OS === 'android' ? 24 : 0
+            }
+
         };
     };
 
