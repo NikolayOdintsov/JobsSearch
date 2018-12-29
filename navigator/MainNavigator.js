3d5c1304c5c643d507e57e10d091dfd9601a27ff
Nikolay Odintsov <nikolay.odintsov@akelius.de>
2018-12-29 16:30:03 +0100

    TabBasr improvements

--- navigator/MainNavigator.js
+++ navigator/MainNavigator.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 import AuthScreen from '../screens/AuthScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import MapScreen from '../screens/MapScreen';
@@ -22,14 +24,43 @@ const ReviewFlowNavigator = createStackNavigator({
 const MainFlowNavigator = createBottomTabNavigator({
     map: {
         screen: MapScreen,
-        navigationOptions: bottomTabNavigationOptions
+        navigationOptions: {
+            ...bottomTabNavigationOptions,
+            title: 'Map',
+            tabBarIcon: ({ tintColor }) => {
+                return (
+                    <Icon name="my-location" size={30} color={tintColor}/>
+                );
+            }
+        }
     },
     deck: {
         screen: DeckScreen,
-        // navigationOptions: bottomTabNavigationOptions
+        navigationOptions: {
+            title: 'Jobs',
+            tabBarIcon: ({ tintColor }) => {
+                return (
+                    <Icon name="description" size={30} color={tintColor}/>
+                );
+            }
+        }
     },
     review: {
-        screen: ReviewFlowNavigator
+        screen: ReviewFlowNavigator,
+        navigationOptions: {
+            title: 'Review',
+            tabBarIcon: ({ tintColor }) => {
+                return (
+                    <Icon name="favorite" size={30} color={tintColor}/>
+                );
+            }
+        }
+    }
+}, {
+    tabBarOptions: {
+        labelStyle: {
+            fontSize: 12,
+        }
     }
 });
 
